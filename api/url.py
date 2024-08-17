c5f324639e692d6d4c755b85d9ed130a02ec64fa
katjes <katjes733@gmx.net>
2024-08-17 12:03:34 -0700

    chore: refactored all apis to have internal component

--- api/url.py
+++ api/url.py
@@ -1,29 +1,40 @@
 """
 URL API
 """
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response
 
 from playhouse.shortcuts import model_to_dict
+
+from peewee import IntegrityError
 
 from models.check_url import RcCheckUrlsModel
 
 url_blueprint = Blueprint('url_blueprint', __name__)
 
 
+def get_urls_from_db() -> list[RcCheckUrlsModel]:
+    """
+    Gets all URLs from the DB.
+
+    Returns:
+        list[RcCheckUrlsModel]: Array of URL dict
+    """
+    query = RcCheckUrlsModel.select()
+    return [model_to_dict(r) for r in query]
+
+
 @url_blueprint.route('/urls', methods=['GET'])
-def get_urls():
+def get_urls() -> Response:
     """
     Gets all the URLs
 
     Returns:
-        dict: Returns an array of dict for type RcCheckUrlsModel
+        Response: Returns an array of dict for type RcCheckUrlsModel
     """
-    query = RcCheckUrlsModel.select()
-    results = [model_to_dict(r) for r in query]
-    return jsonify(results)
+    return jsonify(get_urls_from_db())
 
 
-def get_url(url: str):
+def get_url(url: str) -> RcCheckUrlsModel:
     """
     Gets a single URL.
 
@@ -31,16 +42,33 @@ def get_url(url: str):
         url (str): The URL to get.
 
     Returns:
-        dict: The URL or none if not found
+        RcCheckUrlsModel: The URL or none if not found
     """
     query = RcCheckUrlsModel.select().where(RcCheckUrlsModel.url == url)
     return query.get_or_none()
 
 
+def delete_url_from_db(url: str) -> dict:
+    """
+    Deletes an URL from the DB if it exists.
+
+    Args:
+        url (str): The URL
+
+    Returns:
+        dict: The deleted URL dict
+    """
+    existing_url = get_url(url)
+    if not existing_url:
+        return None
+    existing_url.delete_instance()
+    return model_to_dict(existing_url)
+
+
 @url_blueprint.route('/urls/<string:url>', methods=['DELETE'])
 def delete_url(url: str):
     """
-    Deletes an URl
+    Deletes an URL
 
     Args:
         url (str): The URL to delete.
@@ -48,16 +76,32 @@ def delete_url(url: str):
     Returns:
         dict: The deleted URL.
     """
-    existing_url = get_url(url)
-    if not existing_url:
+    deleted_url = delete_url_from_db(url)
+    if not deleted_url:
         return jsonify({"error": f"URL [{url}] does not exist."}), 404
+    return jsonify(deleted_url), 200
 
-    existing_url.delete_instance()
-    return jsonify(model_to_dict(existing_url)), 200
+
+def create_url_in_db(data: dict) -> dict:
+    """
+    Creates an URL record in the DB.
+
+    Args:
+        data (dict): The data dict.
+
+    Returns:
+        dict: The created URL dict or a dict with the error.
+    """
+    try:
+        created_url = RcCheckUrlsModel.create(**data)
+        created_url.save()
+        return model_to_dict(created_url)
+    except IntegrityError as e:
+        return {"error": str(e)}
 
 
 @url_blueprint.route('/urls', methods=['POST'])
-def create_url(data=None):
+def create_url():
     """
     Creates an URL. Data can be passed in as function
     parameter or as request payload.
@@ -68,32 +112,47 @@ def create_url(data=None):
     Returns:
         dict: The created URL object.
     """
-    if data is None:
-        data = {}
-    url_data = {}
-    if data and RcCheckUrlsModel.url.name in data and RcCheckUrlsModel.url_description.name in data:
-        url_data[RcCheckUrlsModel.url.name] = data[RcCheckUrlsModel.url.name]
-        url_data[RcCheckUrlsModel.url_description.name] = data[RcCheckUrlsModel.url_description.name]
-    else:
-        tmp_data = request.get_json()
-        if tmp_data and RcCheckUrlsModel.url.name in tmp_data and RcCheckUrlsModel.url_description.name in tmp_data:
-            url_data[RcCheckUrlsModel.url.name] = tmp_data[RcCheckUrlsModel.url.name]
-            url_data[RcCheckUrlsModel.url_description.name] = tmp_data[RcCheckUrlsModel.url_description.name]
-    if not url_data:
-        return jsonify({"error": "Invalid URL properties."}), 400
-    existing_url = get_url(url_data[RcCheckUrlsModel.url.name])
-    if existing_url:
-        return jsonify({"error": f"URL [{url_data[RcCheckUrlsModel.url.name]}] already exists."}), 404
-    new_url = RcCheckUrlsModel.create(
-        url=url_data[RcCheckUrlsModel.url.name],
-        url_description=url_data[RcCheckUrlsModel.url_description.name],
-    )
-    new_url.save()
-    return jsonify(model_to_dict(new_url)), 200
+    url_data = request.get_json()
+    created_url = create_url_in_db(url_data)
+    if "error" in created_url and "duplicate key" in created_url["error"]:
+        return jsonify({
+            "error": f"URL [{url_data[RcCheckUrlsModel.url.name]}] "
+            "already exists.",
+            "details": created_url["error"],
+        }), 404
+    if "error" in created_url and \
+            "Failing row contains" in created_url["error"]:
+        return jsonify({
+            "error": "Invalid URL properties.",
+            "details": created_url["error"],
+        }), 400
+    return jsonify(created_url), 200
+
+
+def update_url_in_db(data: dict) -> dict:
+    """
+    Updates a record in the DB.
+
+    Args:
+        data (dict): the URL data object
+
+    Returns:
+        dict: Th updated URL data object.
+    """
+    if "url" not in data:
+        return {"error": "Object contains no mandatory element 'url'."}
+    updated_url = get_url(data["url"])
+    if not updated_url:
+        return {"error": f"URL [{data['url']}] does not exist."}
+    for key, value in data.items():
+        if hasattr(updated_url, key):
+            setattr(updated_url, key, value)
+    updated_url.save()
+    return model_to_dict(updated_url)
 
 
 @url_blueprint.route('/urls/<string:url>', methods=['PUT'])
-def update_url(url: str, data=None):
+def update_url(url: str):
     """
     Update an URL. Data can be passed as function parameter
     or as request payload.
@@ -105,20 +164,11 @@ def update_url(url: str, data=None):
     Returns:
         dict: The updated URL object.
     """
-    if data is None:
-        data = {}
-    url_data = {}
-    if data and RcCheckUrlsModel.url_description.name in data:
-        url_data[RcCheckUrlsModel.url_description.name] = data[RcCheckUrlsModel.url_description.name]
-    else:
-        tmp_data = request.get_json()
-        if tmp_data and RcCheckUrlsModel.url_description.name in tmp_data:
-            url_data[RcCheckUrlsModel.url_description.name] = tmp_data[RcCheckUrlsModel.url_description.name]
-    if not url_data:
-        return jsonify({"error": "Invalid URL properties."}), 400
-    updated_url = get_url(url)
-    if not updated_url:
-        return jsonify({"error": f"URL [{url}] does not exist."}), 404
-    updated_url.url_description = url_data[RcCheckUrlsModel.url_description.name]
-    updated_url.save()
-    return jsonify(model_to_dict(updated_url)), 200
+    data = request.get_json()
+    data["url"] = url
+    updated_url = update_url_in_db(data)
+    if "error" in updated_url and "Object contains no" in updated_url["error"]:
+        return jsonify(updated_url), 400
+    if "error" in updated_url and "does not exist" in updated_url["error"]:
+        return jsonify(updated_url), 404
+    return jsonify(updated_url), 200
